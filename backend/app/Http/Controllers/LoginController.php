22f3baca5d9236f15b7e08a0d822ada10eec9a58
manthinh <kbkl0224@i.softbank.jp>
2022-03-23 15:41:56 +0900

    ログイン機能パワープレイ

--- backend/app/Http/Controllers/LoginController.php
+++ backend/app/Http/Controllers/LoginController.php
@@ -19,14 +19,27 @@ class LoginController extends controller
     {
         $id1 = $request->input('user_id');
         $pass1 = $request->input('password');
-        $id2 = User::where('id','<',3)->get()->pluck('name','password');
-        // dd($id2);        
+        $id2 = User::where('id',1)->get()->pluck('name');
+        $id3 = User::where('id',1)->get()->pluck('password');
+        $id4 = User::where('id',2)->get()->pluck('name');
+        $id5 = User::where('id',2)->get()->pluck('password');
+        // dd($id2,$id3);        
         foreach($id2 as $name)
-        // dd($name);
-        if($id2 === 0)
-    {
-        return view('login', ['login_error' => '1']);
-    }
+        foreach($id3 as $password)
+        foreach($id4 as $name2)
+        foreach($id5 as $password2)
+        // dd($name,$password);
+        if($id1 === $name && $pass1 === $password)
+        {
+            echo "ログイン成功";
+        } else if($id1 === $name2 && $pass1 === $password2) 
+        {
+            echo "ログイン成功";
+        } else 
+        {
+            echo "ユーザー名とパスワードが一致しません";
+        }
+    
     }
 }
     // public function pass(Request $request)
